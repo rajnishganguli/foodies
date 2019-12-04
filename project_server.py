26d4c6f85cfa4cc4cdcde2ec13a003f85a5d80ee
Rajnish <rkgangulii@gmail.com>
2019-12-04 17:51:44 +0530

    Added all the routes for the app

--- project_server.py
+++ project_server.py
@@ -4,8 +4,45 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "hello"
+@app.route('/restaurants')
+def show_restaurants():
+    return "This page will show all restaurants"
+
+
+@app.route('/restaurant/new')
+def new_restaurant():
+    return "This page will be used to make new restaurants"
+
+
+@app.route('/restaurant/<int:restaurant_id>/edit')
+def edit_restaurant(restaurant_id):
+    return "This page will be used to edit a restaurant"
+
+
+@app.route('/restaurant/<int:restaurant_id>/delete')
+def delete_restaurant(restaurant_id):
+    return "This page will be used to delete an existing restaurant"
+
+
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def show_menu(restaurant_id):
+    return "This page will be used to show menu of the given restaurant"
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def new_menu_item(restaurant_id):
+    return "This page will be used to add a new menu item"
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def edit_menu_item(restaurant_id, menu_id):
+    return "This page will be used to update a menu item of the given restaurant"
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def delete_menu_item(restaurant_id, menu_id):
+    return "This page will be used to delete a menu item of the given restaurant"
 
 
 if __name__ == '__main__':
