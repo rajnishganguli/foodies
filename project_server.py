c3102b16e10dca433b16912a6a4ee39ee8ad8aba
rajnishganguli <rkgangulii@gmail.com>
2020-02-11 22:54:55 +0530

    Removed initial dummy data that were commented out.

--- project_server.py
+++ project_server.py
@@ -17,23 +17,8 @@ app = Flask(__name__)
 # GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
 from credentials import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_DISCOVERY_URL
 
-# # Fake Restaurants
-# restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-#
-# restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'},
-#                {'name': 'Blue Burgers', 'id': '2'},
-#                {'name': 'Taco Hut', 'id': '3'}]
-#
-# # Fake Menu Items
-# item = {'name': 'Cheese Pizza', 'description': 'made with fresh cheese', 'price': '$5.99', 'course': 'Entree'}
-#
-# items = [
-#     {'name': 'Cheese Pizza', 'description': 'made with cheese', 'price': '$5.99', 'course': 'Entree', 'id': '1'},
-#     {'name': 'Chocolate Cake', 'description': 'made with Choco', 'price': '$3.99', 'course': 'Dessert', 'id': '2'},
-#     {'name': 'Caesar Salad', 'description': 'with fresh vegetables', 'price': '$5.99', 'course': 'Entree', 'id': '3'},
-#     {'name': 'Iced Tea', 'description': 'with lemon', 'price': '$.99', 'course': 'Beverage', 'id': '4'},
-#     {'name': 'Spinach Dip', 'description': 'with spinach', 'price': '$1.99', 'course': 'Appetizer', 'id': '5'}]
 
+# Initializing the database engine.
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
